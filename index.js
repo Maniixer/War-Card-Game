4807b18c0a7840cdb0d845f7e26473404f65d66c
Maniixer <92598085+Maniixer@users.noreply.github.com>
2023-10-13 11:22:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,50 +11,46 @@ let computerPoint = 0;
 newDeck.addEventListener("click", getNewDeckData);
 drawCard.addEventListener("click", drawCards);
 
-function getNewDeckData() {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-      deckId = data.deck_id;
-      drawCard.disabled = false;
-      remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
+async function getNewDeckData() {
+  const resp = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/");
+  const data = await resp.json();
+  console.log(data);
+  deckId = data.deck_id;
+  drawCard.disabled = false;
+  remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
 
-      playerPoint = 0;
-      computerPoint = 0;
-      playerScore.textContent = `Player Score: ${playerPoint}`;
-      computerScore.textContent = `Computer Score: ${computerPoint}`;
-      cardWinner.textContent = "Game of War!";
-    });
+  playerPoint = 0;
+  computerPoint = 0;
+  playerScore.textContent = `Player Score: ${playerPoint}`;
+  computerScore.textContent = `Computer Score: ${computerPoint}`;
+  cardWinner.textContent = "Game of War!";
 }
 
-function drawCards() {
-  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    .then((resp) => resp.json())
-    .then((data) => {
-      if (data.remaining) {
-        document.getElementById(
-          "cards-container"
-        ).children[0].innerHTML = `<div class="img"><img src="${data.cards[0].image}" alt="${data.cards[0].value} OF ${data.cards[0].suit}"></div>`;
-        document.getElementById(
-          "cards-container"
-        ).children[1].innerHTML = `<div class="img"><img src="${data.cards[1].image}" alt="${data.cards[1].value} OF ${data.cards[1].suit}"></div>`;
+async function drawCards() {
+  const resp = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
+  const data = await resp.json();
+  if (data.remaining) {
+    document.getElementById(
+      "cards-container"
+    ).children[0].innerHTML = `<div class="img"><img src="${data.cards[0].image}" alt="${data.cards[0].value} OF ${data.cards[0].suit}"></div>`;
+    document.getElementById(
+      "cards-container"
+    ).children[1].innerHTML = `<div class="img"><img src="${data.cards[1].image}" alt="${data.cards[1].value} OF ${data.cards[1].suit}"></div>`;
 
-        cardWinner.textContent = determineCardWinner(data.cards[0], data.cards[1]);
-        remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
-      } else {
-        remainingCards.textContent = `Cards Remaining: No more cards - Get a new deck!`;
-        drawCard.disabled = true;
+    cardWinner.textContent = determineCardWinner(data.cards[0], data.cards[1]);
+    remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
+  } else {
+    remainingCards.textContent = `Cards Remaining: No more cards - Get a new deck!`;
+    drawCard.disabled = true;
 
-        if (playerPoint > computerPoint) {
-          cardWinner.textContent = "You won the game!";
-        } else if (playerPoint < computerPoint) {
-          cardWinner.textContent = "The computer won the game!";
-        } else {
-          cardWinner.textContent = "It's a tie game!";
-        }
-      }
-    });
+    if (playerPoint > computerPoint) {
+      cardWinner.textContent = "You won the game!";
+    } else if (playerPoint < computerPoint) {
+      cardWinner.textContent = "The computer won the game!";
+    } else {
+      cardWinner.textContent = "It's a tie game!";
+    }
+  }
 }
 
 function determineCardWinner(player, computer) {
